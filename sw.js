564aedec9eb38908a8cdc5046fa517b0b96fdfdc
ki325ko <ki325ko@gmail.com>
2026-06-17 08:06:55 +0900

    fix: 完了切替・クライアント状態変更でupdatedAtを更新（完了/今日/達成の状態が同期されない不具合）

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kanrilog-v11';
+const CACHE = 'kanrilog-v12';
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./', './index.html'])));
   self.skipWaiting();
