943d4c0f186cc751a9105f0b222d59975efc0903
ki325ko <ki325ko@gmail.com>
2026-06-24 22:27:33 +0900

    style: グループ名を大きく・ヘッダー下に仕切り線を追加してタスクと区別

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kanrilog-v18';
+const CACHE = 'kanrilog-v19';
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./', './index.html'])));
   self.skipWaiting();
